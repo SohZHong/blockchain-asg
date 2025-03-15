f475d03466cf7b32ea7cc71b073355227d068a1e
SohZHong <sohzhehong09@gmail.com>
2025-03-15 18:10:20 +0800

    deployed entrypont for alfajores testnet

---
+++ packages/hardhat/ignition/modules/EntryPointModule.ts
@@ -0,0 +1,9 @@
+import { buildModule } from '@nomicfoundation/hardhat-ignition/modules';
+
+const EntryPointModule = buildModule('EntryPointModule', (m) => {
+  const entryPoint = m.contract('CeloEntryPoint');
+
+  return { entryPoint };
+});
+
+export default EntryPointModule;

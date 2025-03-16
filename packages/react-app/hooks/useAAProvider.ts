fefe2d691bacab1dc59573da3244ffdb32b391de
SohZHong <sohzhehong09@gmail.com>
2025-03-16 18:06:02 +0800

    removed unused variables

--- packages/react-app/hooks/useAAProvider.ts
+++ packages/react-app/hooks/useAAProvider.ts
@@ -37,7 +37,6 @@ export function useAAProvider() {
           factoryAddress,
           paymasterAPI,
         });
-        console.log(aaWalletInstance);
 
         setAAWallet(aaWalletInstance);
       } catch (error) {

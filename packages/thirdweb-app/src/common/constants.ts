e1e7a6149591b7fa9ec993495fe1c6203dcf7ed6
SohZHong <sohzhehong09@gmail.com>
2025-04-02 21:07:36 +0800

    refactor: updated event factory address

--- packages/thirdweb-app/src/common/constants.ts
+++ packages/thirdweb-app/src/common/constants.ts
@@ -18,7 +18,7 @@ const managerAddress = "0xe0dBc74bB3795f69b763629752c27DF2e58d6f58";
 // Contract for organiser management
 const organiserAddress = "0xfb8672FDF496B66FB81b43B1b1cF1938CA7fb71e";
 // Contract for event factory
-const eventFactoryAddress = "0x8f986dBE23523cDEC3d4d69124b5ff250EdE9C36";
+const eventFactoryAddress = "0x26cdc1B349e50A8793Cbd1C5749334Bb39CccD62";
 
 export {
   chain,

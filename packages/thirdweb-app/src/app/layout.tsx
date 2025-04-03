7c4aa99da1d426e60853bb7022f06937cb6b8763
Sean Hoe Kai Zher <102246906+Seann2003@users.noreply.github.com>
2025-04-03 15:42:53 +0800

    Fixed duplicate provider

--- packages/thirdweb-app/src/app/layout.tsx
+++ packages/thirdweb-app/src/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ThirdwebProvider } from "thirdweb/react";
-import { Toaster } from "@/components/ui/sonner";
+import { Providers } from "./providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,8 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ThirdwebProvider>{children}</ThirdwebProvider>
-        <Toaster />
+        <Providers>{children}</Providers>
       </body>
     </html>
   );

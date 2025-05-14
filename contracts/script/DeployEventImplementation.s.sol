3df1bc802fef0fdcbb5b25b5a43b281d2df8f1b7
SohZHong <sohzhehong09@gmail.com>
2025-05-14 21:42:12 +0800

    deployed event implementation

--- contracts/script/DeployEventImplementation.s.sol
+++ contracts/script/DeployEventImplementation.s.sol
@@ -6,24 +6,26 @@ import {EventImplementation} from "../src/EventImplementation.sol";
 import {EventFactory} from "../src/EventFactory.sol";
 
 contract DeployEventImplementation is Script {
-    function run() public returns (EventImplementation, EventFactory) {
+    function run() public {
         // Start broadcasting transactions
+        vm.createSelectFork("alfajores");
         vm.startBroadcast();
 
         // Step 1: Deploy EventImplementation first
         EventImplementation eventImplementation = new EventImplementation();
 
         // Replace this with the actual organizer token address
-        address organizerTokenAddress = 0xfb8672FDF496B66FB81b43B1b1cF1938CA7fb71e;
+        address organizerTokenAddress = 0xFa1946Ae5C5cc2b07419D307F727484b52C9A6c1;
 
         // Step 2: Deploy EventFactory
         EventFactory eventFactory = new EventFactory();
 
         // Step 3: Call initialize() on EventFactory
-        eventFactory.initialize(address(eventImplementation), organizerTokenAddress);
+        eventFactory.initialize(
+            address(eventImplementation),
+            organizerTokenAddress
+        );
 
         vm.stopBroadcast();
-
-        return (eventImplementation, eventFactory);
     }
 }

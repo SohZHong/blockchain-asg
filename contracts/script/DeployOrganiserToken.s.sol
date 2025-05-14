1f92ca57ac31b42eecfea10c7b31e689d8b11bc3
SohZHong <sohzhehong09@gmail.com>
2025-05-14 21:20:24 +0800

    fixed deployment issues

--- contracts/script/DeployOrganiserToken.s.sol
+++ contracts/script/DeployOrganiserToken.s.sol
@@ -5,16 +5,15 @@ import {Script} from "forge-std/Script.sol";
 import {OrganiserToken} from "../src/OrganiserToken.sol";
 
 contract DeployOrganiserToken is Script {
-    function run() public returns (OrganiserToken) {
+    function run() public {
         // Start broadcasting transactions
+        vm.createSelectFork("alfajores");
         vm.startBroadcast();
 
         // Step 1: Deploy OrganiserToken first
-        OrganiserToken organiserToken = new OrganiserToken("ipfs://bafybeiawnhynmc7iqgelc5ro7chmxewnwn5hzkxpfhbefmkx4wykstmdxa/");
-
+        new OrganiserToken(
+            "ipfs://bafybeiawnhynmc7iqgelc5ro7chmxewnwn5hzkxpfhbefmkx4wykstmdxa/"
+        );
         vm.stopBroadcast();
-
-        return (organiserToken);
     }
 }
-

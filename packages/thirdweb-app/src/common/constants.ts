74f0834aa02821bc23075eaa6399f569bf1c7962
SohZHong <sohzhehong09@gmail.com>
2025-05-15 21:25:45 +0800

    modified: address

--- packages/thirdweb-app/src/common/constants.ts
+++ packages/thirdweb-app/src/common/constants.ts
@@ -14,13 +14,13 @@ const appUrl = "https://example.com";
 const ownerAddress = "0x42d0c62B46372491F1bb7C494c43A8469EEd5224";
 
 // Contract for match making
-const managerAddress = "0xe0dBc74bB3795f69b763629752c27DF2e58d6f58";
+const managerAddress = "0xaBbd0129C6d83e54fFe8DF446A8F4964550dB1AC";
 const nftAddress = "0xc61BF2E3cD2E9C25619aAb85f516E7160f4e31c0";
 
 // Contract for organiser management
-const organiserAddress = "0xfb8672FDF496B66FB81b43B1b1cF1938CA7fb71e";
+const organiserAddress = "0xfa1946ae5c5cc2b07419d307f727484b52c9a6c1";
 // Contract for event factory
-const eventFactoryAddress = "0x26cdc1B349e50A8793Cbd1C5749334Bb39CccD62";
+const eventFactoryAddress = "0x05e75f1c03b6dd8dfac975c699ef692bbba718d9";
 
 export {
   chain,
